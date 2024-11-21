849f553bf608dcff876c6e35ac589db5d879d672
kshitij-hash <kjangra1557@gmail.com>
2024-11-22 01:34:40 +0530

    fix: middleware fixes

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,24 +1,7 @@
 import Google from "next-auth/providers/google"
 import type { NextAuthConfig } from "next-auth"
-import Credentials from "next-auth/providers/credentials"
  
 export default { providers: [Google({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-}), Credentials({
-    credentials: {
-        email: {},
-        password: {}
-    }, authorize: async (credentials) => {
-        let user = null;
-        
-        const pwHash = saltAndHashPassword(credentials.password);
-        user = await getUserFromDb(credentials.email, pwHash);
-
-        if(!user) {
-            throw new Error("Invalid credentials");
-        }
-
-        return user;
-    }
-})], } satisfies NextAuthConfig
+})] } satisfies NextAuthConfig
---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import authConfig from "@/auth.config"
+import NextAuth from "next-auth"
+import { PUBLIC_ROUTES, AUTH_ROUTES, AUTH_PREFIX, DEFAULT_LOGIN_REDIRECT } from "@/routes"
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(AUTH_PREFIX)
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname)
+  const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname)
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if(isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return;
+  }
+
+  if(!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/login", nextUrl))
+  }
+
+  return;
+})
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+}
