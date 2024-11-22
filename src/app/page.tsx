694d6a9f2fa2a9f4d06b4a89609e95b8b570790f
kshitij-hash <kjangra1557@gmail.com>
2024-11-22 22:27:14 +0530

    chore: added get started on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="container flex justify-center items-center flex-col min-h-[90vh] mx-auto p-4 max-w-4xl">
       {/* //* LANDING PAGE (PUBLIC ROUTE) */}
       <h1 className="text-3xl font-bold mb-8">Time Management App</h1>
+      <Button className="font-normal" asChild>
+        <Link href="/login">Get started</Link>
+      </Button>
     </main>
   )
 }
