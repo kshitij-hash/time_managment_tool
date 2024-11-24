c0650d2e0e3ee1b20696dd7f80068a70d5530a6d
kshitij-hash <kjangra1557@gmail.com>
2024-11-24 20:59:33 +0530

    chore: removed unnecessary imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,9 @@ import type { Metadata } from "next"
 
 import "@/app/globals.css"
 import { poppins } from "@/lib/utils/fonts"
-import localFont from "next/font/local"
 import "./globals.css"
 import { NavBar } from "./components/Navbar"
 import { Toaster } from "@/components/ui/toaster"
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-})
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
